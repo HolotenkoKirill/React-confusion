94a05a4b54b92797a8fa188c7d7069e00c67043a
Holotenko Kirill <Kirya.golotenko@gmail.com>
2020-12-27 16:30:21 +0200

    Components part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/ManuComponent.js';
+import { DISHES } from './shared/dishes.js';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-      <div className="container">
-      <NavbarBrand href="/">Restirante Con Fusion</NavbarBrand>
-      </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+
+    render() {
+        return (
+			<div>
+		      	<Navbar dark color="primary">
+		      		<div className="container">
+		      			<NavbarBrand href="/">Restirante Con Fusion</NavbarBrand>
+		      		</div>
+		      	</Navbar>
+		      	<Menu dishes={this.state.dishes} />
+		    </div>
+		);
+    }
+
 }
 
 export default App;
